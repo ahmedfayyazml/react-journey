3ebebb6e8a809aa55a7112ca2cb60e643e6d90a3
ahmedfyaz <awanahmed755@gmail.com>
2025-12-14 10:48:01 +0500

    started writing css

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -6,8 +6,8 @@ const App = () => {
         <div className={"parent"}>
             <div className={'card'}>
                 <div className={'top'}>
-                    <img src={"https://www.logomagicians.com/wp-content/uploads/2024/01/The-Smile-and-Arrow-Logo-from-2000-till-Present.webp"} alt = ""/>
-                    <button>Save <Bookmark /></button>
+                    <img src={"https://imgs.search.brave.com/1SmDks4CPsuEM1txgXrUz2cuSfM8XLjvrvTIacq4V2s/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvODYwYzA4NzE1/OTE2NzM1YTQ1OWNj/Zjg0MmI0Y2U2YzI2/ODE5NmZhNGZjODA4/N2Y0NmI1ZmU4ZWQy/ZmM2MjdmMS93d3cu/YW1hem9uLmNvbS8"} alt = ""/>
+                    <button>Save <Bookmark size={11}/></button>
                 </div>
                 <div className={'center'}>
                     <h3>Amazon <span>5 days ago</span></h3>
@@ -29,3 +29,7 @@ const App = () => {
     )
 }
 export default App
+
+
+
+// https://imgs.search.brave.com/FUWEcyC8ISzobxF7cnrKQSk_Uegq2QmN0YZ7tWQaoIU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi9iL2I5LzIw/MjNfRmFjZWJvb2tf/aWNvbi5zdmcvMjUw/cHgtMjAyM19GYWNl/Ym9va19pY29uLnN2/Zy5wbmc
