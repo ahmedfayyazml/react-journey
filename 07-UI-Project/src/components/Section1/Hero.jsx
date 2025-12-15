77f5bb0037ae44bef1aa82ee7d042b3fd263347b
ahmedfyaz <awanahmed755@gmail.com>
2025-12-16 00:41:11 +0500

    finished left side content

---
+++ 07-UI-Project/src/components/Section1/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className={'p-6'}>
+            <h3 className={'text-5xl font-bold mb-7 leading-[1.1]'}>Prospective<br/><span className={'text-gray-600'}>customer</span><br/>segmentation</h3>
+            <p className={'text-xl font-medium text-gray-600'}> lorem ipsum dolor sit amet consectetur lorem ipsum dolor sit amet consectetur lorem ipsum dolor sit amet consectetur </p>
+        </div>
+    )
+}
+export default Hero
--- 07-UI-Project/src/components/Section1/LeftContent.jsx
+++ 07-UI-Project/src/components/Section1/LeftContent.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
+import Hero from "./Hero.jsx";
 
 const LeftContent = () => {
     return (
         <div className={'h-full flex-col justify-between w-1/4'}>
-            <div className={'p-6'}>
-                <h3 className={'text-5xl font-bold mb-7 leading-[1.1]'}>Prospective<br/><span className={'text-gray-600'}>customer</span><br/>segmentation</h3>
-                <p className={'text-xl font-medium text-gray-600'}> lorem ipsum dolor sit amet consectetur lorem ipsum dolor sit amet consectetur lorem ipsum dolor sit amet consectetur </p>
-            </div>
+            <Hero/>
             <div className={'h-1/6'}>
             </div>
             <div className={'h-1/12'}></div>
