89ec1fef3ee59988065df9a128f5e991c70d3d4b
ahmedfyaz <awanahmed755@gmail.com>
2025-12-21 18:45:29 +0500

    chap 10 completed

---
+++ 10-useState-Advance/src/App.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+const App = () => {
+
+    const [num, setNum] = useState(0);
+    function onKick(){
+        setNum(prevState => prevState + 1);
+        setNum(prevState => prevState + 1);
+        setNum(prevState => prevState + 1);
+
+    }
+    return (
+        <div>
+            <h1>{num}</h1>
+            <button onClick={onKick}>Click</button>
+        </div>
+    )
+}
+export default App
