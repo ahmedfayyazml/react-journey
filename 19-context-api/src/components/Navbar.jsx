eb83833de7ff0fc28c736a0ad0f96a7247444c82
ahmedfyaz <awanahmed755@gmail.com>
2026-01-01 21:44:10 +0500

    chap 19 started compnents are made

---
+++ 19-context-api/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className={'nav'}>
+            <h2>Sheriyans</h2>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+export default Navbar
