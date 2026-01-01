67018ff6b1d9a99b1d6df98781db3073e6f889df
ahmedfyaz <awanahmed755@gmail.com>
2026-01-01 21:14:20 +0500

    chap 18 started + Happy new Year

---
+++ 18-bonus-lecture/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+const Navbar = (props) => {
+
+    function changeTheme() {
+        console.log("Theme Change ",props.theme)
+        props.setTheme('Dark')
+    }
+    return (
+        <div>
+            <p>{props.theme}</p>
+            <button onClick={changeTheme}>Change theme</button>
+        </div>
+    )
+}
+export default Navbar
